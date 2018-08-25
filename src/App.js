aaba3276b0338673a8878f82d3b21655f81905ab
saleh <saleh@fatimahs-iMac.local>
2018-08-25 09:55:17 +0300

    revert change for fetchUserData i.e remove generateHeaders promise

--- src/App.js
+++ src/App.js
@@ -61,30 +61,31 @@ class App extends Component {
   }
 
   fetchUserData () {
-    this.generateHeaders().then(headers => {
-      const user = netlifyIdentity.currentUser()
-      if (user) {
-        if (user.app_metadata && user.app_metadata.faunadb_ref) {
-          const { faunadb_ref } = user.app_metadata
-          const myAuthHeader = `Bearer ${user.token.access_token}`
-          return fetch(`/.netlify/functions/donors-read/${faunadb_ref}`, {
-            headers
-          })
-            .then(response => {
-              if (!response.ok) {
-                console.log('aww, not ok')
-                return Promise.resolve({})
-              }
-              return response.json()
-            })
-            .catch(e => {
-              console.log(e)
+    const user = netlifyIdentity.currentUser()
+    if (user) {
+      if (user.app_metadata && user.app_metadata.faunadb_ref) {
+        const { faunadb_ref } = user.app_metadata
+        const myAuthHeader = `Bearer ${user.token.access_token}`
+        return fetch(`/.netlify/functions/donors-read/${faunadb_ref}`, {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: myAuthHeader
+          }
+        })
+          .then(response => {
+            if (!response.ok) {
+              console.log('aww, not ok')
               return Promise.resolve({})
-            })
-        }
+            }
+            return response.json()
+          })
+          .catch(e => {
+            console.log(e)
+            return Promise.resolve({})
+          })
       }
-      return Promise.resolve({})
-    })
+    }
+    return Promise.resolve({})
   }
 
   handleSubmit = (event, fields) => {
